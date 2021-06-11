687621464a1c82a565561938cb7feb30a211d55c
Nailul Firdaus <nail.nafir@gmail.com>
2021-06-11 09:02:33 +0700

    unlock assets in pubspec

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,12 +3,42 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  String greeting() {
+    var time = DateTime.now().hour;
+
+    if (time < 10) {
+      return "Pagi";
+    } else if (time < 15) {
+      return "Siang";
+    } else if (time < 18) {
+      return "Sore";
+    } else {
+      return "Malam";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home Page"),
-      ),
-    );
+        body: Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 250,
+          color: Color(0xFF133EFF),
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  Text("Selamat ${greeting()},"),
+                  Text("Nailul Firdaus!"),
+                ],
+              ),
+              // Image.asset(""),
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
