a346dca9fedbbadba30fd665fa8a718b035e744c
Nailul Firdaus <nail.nafir@gmail.com>
2021-06-11 08:30:20 +0700

    add text theme main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(textTheme: GoogleFonts.montserratTextTheme()),
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
